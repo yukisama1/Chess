b21db2bd99bb1134c2c1606aa2e4b43d272d5c51
yukisama1 <152201388+yukisama1@users.noreply.github.com>
2024-10-16 23:40:48 +0200

    first steps for movement

--- chess/src/board.rs
+++ chess/src/board.rs
@@ -1,13 +1,12 @@
 use crate::pieces;
 
 pub fn build_board() -> [[Option<pieces::Piece>; 8]; 8] {
-      let mut board = [[None; 8]; 8];
+      let board = [[None; 8]; 8];
 
       board
 } 
 
 pub fn add_all_pieces(mut board: [[Option<pieces::Piece>; 8]; 8]) -> [[Option<pieces::Piece>; 8]; 8] {
-      let count = 0;
       for y in 0..8 {
             for x in 0..8 {
                   if y > 1 && y < 6 {
@@ -20,22 +19,37 @@ pub fn add_all_pieces(mut board: [[Option<pieces::Piece>; 8]; 8]) -> [[Option<pi
             }
       }
 
-      // board[0][0] = pieces::set_piece(board[0][0].unwrap(), pieces::Figure::Rook);
-      // board[0][1] = pieces::set_piece(board[0][0].unwrap(), pieces::Figure::Knight);
-      // board[0][2] = pieces::set_piece(board[0][0].unwrap(), pieces::Figure::Bishop);
-      // board[0][3] = pieces::set_piece(board[0][0].unwrap(), pieces::Figure::Queen);
-      // board[0][4] = pieces::set_piece(board[0][0].unwrap(), pieces::Figure::King);
-      // board[0][5] = pieces::set_piece(board[0][0].unwrap(), pieces::Figure::Bishop);
-      // board[0][6] = pieces::set_piece(board[0][0].unwrap(), pieces::Figure::Knight);
-      // board[0][7] = pieces::set_piece(board[0][0].unwrap(), pieces::Figure::Rook);
-      // board[7][0] = pieces::set_piece(board[0][0].unwrap(), pieces::Figure::Rook);
-      // board[7][1] = pieces::set_piece(board[0][0].unwrap(), pieces::Figure::Knight);
-      // board[7][2] = pieces::set_piece(board[0][0].unwrap(), pieces::Figure::Bishop);
-      // board[7][3] = pieces::set_piece(board[0][0].unwrap(), pieces::Figure::Queen);
-      // board[7][4] = pieces::set_piece(board[0][0].unwrap(), pieces::Figure::King);
-      // board[7][5] = pieces::set_piece(board[0][0].unwrap(), pieces::Figure::Bishop);
-      // board[7][6] = pieces::set_piece(board[0][0].unwrap(), pieces::Figure::Knight);
-      // board[7][7] = pieces::set_piece(board[0][0].unwrap(), pieces::Figure::Rook);
+      board[0][0] = pieces::set_piece(board[0][0].unwrap(), pieces::Figure::Rook);
+      board[0][1] = pieces::set_piece(board[0][1].unwrap(), pieces::Figure::Knight);
+      board[0][2] = pieces::set_piece(board[0][2].unwrap(), pieces::Figure::Bishop);
+      board[0][3] = pieces::set_piece(board[0][3].unwrap(), pieces::Figure::Queen);
+      board[0][4] = pieces::set_piece(board[0][4].unwrap(), pieces::Figure::King);
+      board[0][5] = pieces::set_piece(board[0][5].unwrap(), pieces::Figure::Bishop);
+      board[0][6] = pieces::set_piece(board[0][6].unwrap(), pieces::Figure::Knight);
+      board[0][7] = pieces::set_piece(board[0][7].unwrap(), pieces::Figure::Rook);
+      board[7][0] = pieces::set_piece(board[7][0].unwrap(), pieces::Figure::Rook);
+      board[7][1] = pieces::set_piece(board[7][1].unwrap(), pieces::Figure::Knight);
+      board[7][2] = pieces::set_piece(board[7][2].unwrap(), pieces::Figure::Bishop);
+      board[7][3] = pieces::set_piece(board[7][3].unwrap(), pieces::Figure::Queen);
+      board[7][4] = pieces::set_piece(board[7][4].unwrap(), pieces::Figure::King);
+      board[7][5] = pieces::set_piece(board[7][5].unwrap(), pieces::Figure::Bishop);
+      board[7][6] = pieces::set_piece(board[7][6].unwrap(), pieces::Figure::Knight);
+      board[7][7] = pieces::set_piece(board[7][7].unwrap(), pieces::Figure::Rook);
 
       board
 }
+
+pub fn move_piece(mut board: [[Option<pieces::Piece>; 8]; 8], orposx: usize, orposy: usize, posx: usize, posy: usize) -> [[Option<pieces::Piece>; 8]; 8]{
+      let mut arraycontent = board[orposy][orposx];
+      // if arraycontent.is_some() {
+      //     let figure = piece.kind;
+      //     if pieces::validate_move(figure, orposx, orposy) {
+            
+      //     } else {
+      //         println!("unvalid move");
+      // //     }
+      // } else {
+  
+      // }
+      board
+  }
--- chess/src/pieces.rs
+++ chess/src/pieces.rs
@@ -1,5 +1,3 @@
-use std::fmt;
-
 #[derive(Copy, Clone, Debug)]
 pub enum Figure {
     Pawn,
@@ -38,4 +36,52 @@ pub fn new_piece(p: Figure, x: usize, y: usize, pl: Player) -> Piece {
 pub fn set_piece(mut piece: Piece, newfigure: Figure) -> Option<Piece> {
     piece.kind = newfigure;
     Some(piece)
+}
+
+pub fn remove_piece(mut arraycontent: Option<Piece>) -> Option<Piece> {
+    arraycontent = None;
+    arraycontent
+}
+
+pub fn validate_move(figure: Figure,orx: usize, ory: usize, x: usize, y: usize) -> bool {
+    if (x < 0) || (x > 7) || (y < 0) || (y > 7) {
+        println!("coordinates out of board range");
+        return false;
+    }
+    
+    let difx = orx.abs_diff(x);
+    let dify = ory.abs_diff(y);
+    
+    // check possible moves one piece after another
+    // Pawn
+    match figure {
+        Figure::Pawn=> println!("test"),
+        Figure::Rook => if (difx > 0 && dify == 0) || (difx == 0 && dify > 0) {
+            return true;
+        } else {
+            return false;
+        },
+        Figure::Knight => if (difx == 2 && dify == 1) || (difx == 1 && dify == 2) {
+            return true;
+        } else {
+            return false;
+        },
+        Figure::Bishop => if difx == dify {
+            return true;
+        } else {
+            return false;
+        },
+        Figure::Queen => if (difx == dify) || (difx > 0 && dify == 0) || (difx == 0 && dify > 0) {
+            return true;
+        } else {
+            return false;
+        },
+        Figure::King => if difx <= 1 && dify <= 1 {
+            return true;
+        } else {
+            return false;
+        },
+    }
+
+    false
 }
